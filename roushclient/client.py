5c08e4774f62380ad8640679b279f9c2971a02de
rpedde <ron@pedde.com>
2012-10-09 07:37:20 -0500

    put back the elipses

--- roushclient/client.py
+++ roushclient/client.py
@@ -484,14 +484,13 @@ class RoushObject(object):
                 format_str = "%s"
                 value = str(v).strip()
 
-                maxlen = 99
                 if widths and k in widths:
                     maxlen = widths[k]
                     if len(value) > widths[k]:
-                        maxlen = widths[k] - 4
+                        value = value[:maxlen - 4] + '...'
                     format_str = "%%-%ds" % widths[k]
 
-                out_str += (format_str + '%c') % (value[:maxlen], separator)
+                out_str += (format_str + '%c') % (value, separator)
         return out_str
 
     def _resolved_value(self, key):
