8c75f0ed0a96674351620550687d235ccee412b7
rpedde <ron@pedde.com>
2012-10-22 10:04:21 -0500

    start of script to build test environment scaffolding

---
+++ make-environment.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# override default endpoint
+# ROUSH_ENDPOINT=http://localhost:8080
+
+for d in {1..9}; do
+    r2 node create --hostname="test-host-${d}" --backend="unprovisioned" --backend_state="unknown"
+done
+
+for d in {1..3}; do
+    r2 cluster create --name="test-cluster-${d}"
+done
+
+r2 node create --hostname="chef-server" --backend="chef-server" --backend_state="ready"
