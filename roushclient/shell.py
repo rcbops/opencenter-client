c41877c176a93018248b72fefe7b23a946f61ca2
Jason Cannavale <jason.cannavale@rackspace.com>
2013-01-18 15:29:43 -0600

    command line interface

--- roushclient/shell.py
+++ roushclient/shell.py
@@ -1,5 +1,5 @@
+#/usr/bin/env python
 import argparse
-import logging
 import os
 import sys
 import httplib2
@@ -8,23 +8,13 @@ import json
 from client import RoushEndpoint
 
 
-class RoushShell(object):
+class RoushShell():
+    endpoint = RoushEndpoint(None)
+
     def get_base_parser(self):
-        parser = argparse.ArgumentParser(
-            prog='Roush',
-            description=__doc__,
-            epilog='See "r3 help COMMAND" '
-                   'for help on a specific command.',
-            add_help=False,
-            formatter_class=HelpFormatter
-        )
-
-        # Global Arguments
-        parser.add_argument('-h', '--help',
-                            action='store_true',
-                            help=argparse.SUPPRESS,
-                            )
-
+        parser = argparse.ArgumentParser(description='Roush CLI',
+                                         prog='r2',
+                                         )
         parser.add_argument('-v', '--verbose',
                             action='store_true',
                             help='Print more verbose output'
@@ -32,67 +22,57 @@ class RoushShell(object):
 
         return parser
 
-    def get_subcommand_parser(self, commands):
+    def get_subcommand_parser(self):
         parser = self.get_base_parser()
-
-        for command in commands:
-            print command
-
-        self.subcommands = commands
-        subparsers = parser.add_subparsers(metavar='<subcommand>')
-
+        self.subcommands = {}
+        subparsers = parser.add_subparsers(help='subcommands')
+        self._get_actions(subparsers)
         return parser
 
+    def _get_actions(self, subparsers):
+        commands = self.endpoint._object_lists.keys()
 
-    def get_schema(self, endpoint):
-        url = ("%s/schema" % endpoint)
-        req = httplib2.Http()
-        resp, content = req.request(url, "GET")
-        data = json.loads(content)
+        for command in commands:
+            schema = self.endpoint.get_schema(command[:-1])
+            arguments = schema.field_schema
+            callback = getattr(self.endpoint, command)
+            desc = callback.__doc__ or ''
+            help = desc.strip().split('\n')[0]
 
-        commands = []
-        for k in data['schema']['objects']:
-            commands.append(k)
+            subparser = subparsers.add_parser(command[:-1],
+                                              help='%s actions' % command[:-1]
+                                              )
 
-        return commands
+            subparser.add_argument(command,
+                                   choices=['list',
+                                   'show',
+                                   'delete',
+                                   'create',
+                                   'filter',
+                                   'update'],
+                                   help='available commands'
+                                   )
+
+            for arg in arguments:
+                subparser.add_argument('--%s' % arg)
+            self.subcommands[command] = subparser
+            subparser.set_defaults(func=callback)
 
     def main(self, argv):
-        try:
-            ep = RoushEndpoint(None)
-        except:
-            print >> sys.stderr, e
-            sys.exit(1)
+        # setup
+        parser = self.get_subcommand_parser()
+        args = parser.parse_args(argv)
 
-        # Get the Roush Server schema
-        available_commands = self.get_schema(ep.endpoint)
+        for command in self.subcommands:
+            try:
+                action = getattr(args, command)
+                obj = command
+            except:
+                continue
 
-        # parse args
-        parser = self.get_base_parser()
-        (options, args) = parser.parse_known_args(argv)
+        if action == "list":
+            print getattr(self.endpoint, obj)
 
-        # build available commands
-        subcommand_parser = self.get_subcommand_parser(available_commands)
-
-        # Handle toplevel --help/-h before attempting to parse
-        # a command from the command line
-        if options.help or not argv:
-            self.do_help(parser, options)
-            return 0
-
-    def do_help(self, parser, args):
-        if getattr(args, 'command', None):
-            if args.command in self.subcommands:
-                self.subcommands[args.command].print_help()
-            else:
-                raise "'%s' is not a valid subcommand" % args.command
-        else:
-            parser.print_help()
-
-
-class HelpFormatter(argparse.HelpFormatter):
-    def start_section(self, heading):
-        heading = '%s%s' % (heading[0].upper(), heading[1:])
-        super(HelpFormatter, self).start_section(heading)
 
 
 def main():
@@ -102,6 +82,5 @@ def main():
         print >> sys.stderr, e
         sys.exit(1)
 
-
 if __name__ == '__main__':
-        main()
+    main()
